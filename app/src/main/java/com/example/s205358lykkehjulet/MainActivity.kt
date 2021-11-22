8fbacc28548db144bddfc14659ffcd04eac0c524
s205358 <s205358@student.dtu.dk>
2021-11-22 09:30:06 +0100

    Add navigation component

--- app/src/main/java/com/example/s205358lykkehjulet/MainActivity.kt
+++ app/src/main/java/com/example/s205358lykkehjulet/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.s205358lykkehjulet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
